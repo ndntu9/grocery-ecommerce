d57053bb44009a3827b5ce1034a5ec68f6c5efc2
ndntu9 <ndntu.629@gmail.com>
2025-03-23 21:53:38 +0700

    làm phần header & banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-function load(path, selector) {
-    fetch(path)
-        .then(function (response) {
-            return response.text();
-        })
-        .then(function (htmls) {
-            $(selector).innerHTML = htmls;
-        })
-        .finally(function () {
-            window.dispatchEvent(new Event('template-loaded'));
-        });
+function load(templates) {
+    const promises = templates.map(function (template) {
+        return fetch(template.path)
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (html) {
+                $(template.selector).innerHTML = html;
+            });
+    });
+
+    Promise.all(promises).then(function () {
+        window.dispatchEvent(new Event('template-loaded'));
+    });
 }
